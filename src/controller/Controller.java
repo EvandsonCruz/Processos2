bcacb4bb2161f1c9be2565540f121f5d5f52e0d1
Evandson <Evandson@192.168.56.1>
2018-08-23 20:47:44 -0300

    listar windows ok

---
+++ src/controller/Controller.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.swing.JOptionPane;
+
+public class Controller {
+
+	public Controller() {
+		super();
+	}
+
+	public static void listar(String so) {
+		if (so.contains("Windows")) {
+
+			String comando = "TASKLIST /FO TABLE";
+			try {
+				Process proc = Runtime.getRuntime().exec(comando);
+				InputStream fluxo = proc.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+			} catch (IOException e) {
+				String erro = e.getMessage();
+				JOptionPane.showMessageDialog(null, erro, "ERRO", JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
+			}
+		}
+		if (so.contains("Linux")) {
+
+			String comando = "TASKLIST /FO TABLE";
+			try {
+				Process proc = Runtime.getRuntime().exec(comando);
+				InputStream fluxo = proc.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+			} catch (IOException e) {
+				String erro = e.getMessage();
+				JOptionPane.showMessageDialog(null, erro, "ERRO", JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,14 @@
+package view;
+
+import controller.Controller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Controller cont = new Controller();
+		String so = System.getProperty("os.name");
+		cont.listar(so);
+
+	}
+
+}
